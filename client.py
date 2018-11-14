47346e1f56261e30275fb191d942d2c575cab937
Engineev <ryw_pub@126.com>
2018-11-14 16:15:24 +0800

    update

---
+++ client.py
@@ -0,0 +1,36 @@
+from xmlrpc.client import ServerProxy
+import argparse
+from typing import *
+
+
+def valid_strategy(strategy_: List[int]) -> bool:
+    strategy = list(strategy_)
+    total = sum(strategy)
+    if total > 100:
+        return False
+    for item in strategy:
+        if item < 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Submit a strategy')
+    parser.add_argument('-s', '--server',
+                        help='server address. Format: ip:port')
+    parser.add_argument('-id', '--identifier',
+                        help='The identifier of this strategy')
+    parser.add_argument('strategy', type=int, nargs='+',
+                        help=
+                        'Your strategy. 10 integers separated by whitespace')
+    args = parser.parse_args()
+
+    addr = 'http://' + args.server
+    with ServerProxy(addr, allow_none=True) as proxy:
+        strategy = list(args.strategy)
+
+        if not valid_strategy(strategy):
+            print("Invalid strategy")
+            exit(-1)
+
+        print('The score you get:', proxy.add(args.identifier, strategy))
---
+++ server.py
@@ -0,0 +1,42 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import argparse
+from typing import *
+
+
+class Server:
+    _rpc_methods_ = ['add']
+
+    def __init__(self, address):
+        self._strategies: Dict[str, List[int]] = {}
+        self._srv = SimpleXMLRPCServer(address,
+                                       allow_none=True, logRequests=False)
+        self._srv.register_function(self.add, 'add')
+
+    def start(self):
+        self._srv.serve_forever()
+
+    def add(self, name: str, strategy: List[int]) -> int:
+        print('add:', name, str(strategy))
+        self._strategies[name] = list(strategy)
+        return self._run(name)
+
+    def _run(self, name: str) -> int:
+        score = 0
+        strategy = self._strategies[name]
+        for k, v in self._strategies.items():
+            if k == name:
+                continue
+            print(k, v)
+            for i in range(0, 10):
+                if strategy[i] > 2 * v[i]:
+                    score += i + 1
+        return score
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int)
+    args = parser.parse_args()
+
+    srv = Server(('', args.port))
+    srv.start()
